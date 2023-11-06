50920377f0aa09d97c57bded66076b773b8fd2bb
Dannyung50 <olaleyedaniel16@gmail.com>
2023-11-06 20:11:12 +0100

    Initial Commit

---
+++ Guess the flag/Guess_the_flagApp.swift
@@ -0,0 +1,17 @@
+//
+//  Guess_the_flagApp.swift
+//  Guess the flag
+//
+//  Created by Olaleye Daniel Ifeoluwa on 06/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct Guess_the_flagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
